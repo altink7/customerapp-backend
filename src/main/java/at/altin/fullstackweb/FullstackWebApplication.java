0da010e917ed027054f865cc915bd52edc7886eb
altin <kelmendialtin48@gmail.com>
2022-08-31 18:54:29 +0200

    Initial commit

---
+++ src/main/java/at/altin/fullstackweb/FullstackWebApplication.java
@@ -0,0 +1,13 @@
+package at.altin.fullstackweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/altin/fullstackweb/FullstackWebApplicationTests.java
@@ -0,0 +1,13 @@
+package at.altin.fullstackweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
