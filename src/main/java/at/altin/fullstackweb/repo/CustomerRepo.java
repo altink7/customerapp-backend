becb34ea3130870994c6c1ec39306ef81aa0b5c7
altin <kelmendialtin48@gmail.com>
2022-09-01 18:37:34 +0200

    Service erweitert

--- src/main/java/at/altin/fullstackweb/repo/CustomerRepo.java
+++ src/main/java/at/altin/fullstackweb/repo/CustomerRepo.java
@@ -4,4 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepo extends JpaRepository<Customer,Long>{
 
+    void deleteCustomerById(Long id);
+
+    Optional findCustomerById(Long id);
 }
--- src/main/java/at/altin/fullstackweb/service/CustomerService.java
+++ src/main/java/at/altin/fullstackweb/service/CustomerService.java
@@ -5,6 +5,8 @@ import at.altin.fullstackweb.repo.CustomerRepo;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 
+import java.util.List;
+
 @Service
 public class CustomerService {
     private final CustomerRepo customerRepo;
@@ -17,5 +19,21 @@ public class CustomerService {
 
     public Customer addCustomer(Customer customer){
         return customerRepo.save(customer);
+    }
+
+    public List<Customer> findAllCustomers(){
+        return customerRepo.findAll();
+    }
+
+    public Customer updateCustomer(Customer customer){
+        return customerRepo.save(customer);
+    }
+
+    public Customer findCustomerById(Long id){
+        return customerRepo.findCustomerById(id).orElseThrow(()-> new UserNotFoundException("User"+id+"not found!"));
+    }
+
+    public void deleteCustomer(Long id){
+        customerRepo.deleteCustomerById(id);
     }
 }
