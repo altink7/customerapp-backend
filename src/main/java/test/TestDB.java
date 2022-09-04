d09e57c195f6109af5b93cdc9e547c89419cd055
altin <kelmendialtin48@gmail.com>
2022-09-04 14:34:25 +0200

    TestData erweitert

--- src/main/java/test/TestDB.java
+++ src/main/java/test/TestDB.java
@@ -6,24 +6,26 @@ import java.util.*;
 
 public class TestDB {
     public static void main(String[] args) {
-        loadCustomersFromCSVFile("src/main/java/test/testfiles/altindb_customer.csv");
+        List<Customer> testData =loadCustomersFromCSVFile("src/main/java/test/testfiles/altindb_customer.csv");
+
+        for(Customer c: testData)  System.out.println(c.toString());
+
     }
 
     public static List<Customer> loadCustomersFromCSVFile(String filename) {
-        List<Customer> customers = new LinkedList<Customer>();
+        List<Customer> customers = new LinkedList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(filename))) {
-
              String line = in.readLine();
+
              if (line != null) {
 
                  while ((line != null)) {
-
-                     System.out.println(line);
                      customers.add(parseCustomer(line));
 
                      line = in.readLine();
                      }
                  }
+
              } catch (IOException e) {
              e.printStackTrace();
         }
@@ -34,8 +36,8 @@ public class TestDB {
         String[] parts = line.split(",");
 
         Customer c= new Customer(parts[4],Boolean.parseBoolean(parts[1]),parts[2],parts[5],parts[3]);
-
         c.setId(Long.valueOf(parts[0]));
+
         return c;
     }
 }
